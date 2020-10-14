b8459c13fb80075608332d5d01ac05a6f5b264a0
Alex Bertrand <alex.m.bertrand@gmail.com>
2020-10-13 20:25:49 -0400

    added deprecation warning and install dotenv package

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,9 @@
 // requires each route file
 const express = require("express");
-const apiWorkoutRoutes = require("./apiWorkoutRoutes")
+// const apiWorkoutRoutes = require("./apiWorkoutRoutes")
 
 const Router = express.Router();
 
-Router.use("/workouts", apiWorkoutRoutes);
+// Router.use("/workouts", apiWorkoutRoutes);
 
 module.exports = Router;
--- server.js
+++ server.js
@@ -13,8 +13,10 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
+mongoose.connect(process.env.MONGODB_URL || "mongodb://localhost/workout", {
   useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
   useFindAndModify: false
 });
 
